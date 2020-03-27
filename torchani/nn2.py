01ea7bf27d4131553dd59f858e2e5b1a24595484
Raimondas Galvelis <r.galvelis@acellera.com>
2020-03-27 02:14:35 +0100

    Clean up Ensemble2

--- torchani/nn2.py
+++ torchani/nn2.py
@@ -131,12 +131,10 @@ class Ensemble2(Ensemble):
 
         assert np.all(np.sort(species.cpu().numpy())[::-1] == species.cpu().numpy())
 
+        atomic_energies = []
+        self._num_energies = []
         self._nns = []
-        self._celu1 = []
-        self._celu3 = []
-        self._celu5 = []
-
-        energies = []
+        self._celus = []
         for i, submodel in enumerate(self):
             nns = reversed(self[i].values())
             js = reversed(range(len(self[i])))
@@ -144,22 +142,22 @@ class Ensemble2(Ensemble):
                 assert len(nn) == 7
                 midx = torch.tensor(np.flatnonzero(species.cpu() == j), device=species.device)
                 if midx.shape[0] > 0:
-                    self._nns.append(nn)
+                    celu = {1: CELU(0.1), 3: CELU(0.1), 5: CELU(0.1)}
                     vector0 = aev.index_select(0, midx)
                     vector1 = torch.matmul(vector0, nn[0].weight.t()) + nn[0].bias
-                    self._celu1.append(CELU(10))
-                    vector2 = self._celu1[-1].forward(vector1)
+                    vector2 = celu[1].forward(vector1)
                     vector3 = torch.matmul(vector2, nn[2].weight.t()) + nn[2].bias
-                    self._celu3.append(CELU(10))
-                    vector4 = self._celu3[-1].forward(vector3)
+                    vector4 = celu[3].forward(vector3)
                     vector5 = torch.matmul(vector4, nn[4].weight.t()) + nn[4].bias
-                    self._celu5.append(CELU(10))
-                    vector6 = self._celu5[-1].forward(vector5)
+                    vector6 = celu[5].forward(vector5)
                     vector7 = torch.matmul(vector6, nn[6].weight.t()) + nn[6].bias
-                    energies.append(vector7.flatten())
-        self._energies = energies
-        energies = torch.cat(energies)
-        energy = torch.sum(energies).reshape(1) / len(self)
+                    energies = vector7.flatten()
+                    atomic_energies.append(energies)
+                    self._num_energies.append(int(energies.shape[0]))
+                    self._nns.append(nn)
+                    self._celus.append(celu)
+        atomic_energies = torch.cat(atomic_energies)
+        energy = torch.sum(atomic_energies).reshape(1) / len(self)
 
         return SpeciesEnergies(species_, energy)
 
@@ -169,18 +167,14 @@ class Ensemble2(Ensemble):
         num_aevs = int(self._aevs.shape[2])
 
         grad_aevs = []
-        for nn, celu1, celu3, celu5, energy in zip(
-                self._nns,
-                self._celu1, self._celu3, self._celu5,
-                self._energies):
-            num_energies = int(energy.shape[0])
-            grad_vec7 = torch.ones((num_energies, 1), dtype=energy.dtype, device=energy.device)
+        for num_energies, nn, celu in zip(self._num_energies, self._nns, self._celus):
+            grad_vec7 = torch.ones((num_energies, 1), dtype=self._aevs.dtype, device=self._aevs.device)
             grad_vec6 = torch.matmul(grad_vec7, nn[6].weight)
-            grad_vec5 = celu5.backward(grad_vec6)
+            grad_vec5 = celu[5].backward(grad_vec6)
             grad_vec4 = torch.matmul(grad_vec5, nn[4].weight)
-            grad_vec3 = celu3.backward(grad_vec4)
+            grad_vec3 = celu[3].backward(grad_vec4)
             grad_vec2 = torch.matmul(grad_vec3, nn[2].weight)
-            grad_vec1 = celu1.backward(grad_vec2)
+            grad_vec1 = celu[1].backward(grad_vec2)
             grad_vec0 = torch.matmul(grad_vec1, nn[0].weight)
             grad_aevs.append(grad_vec0)
         grad_aevs = torch.cat(grad_aevs)
